c4a62afcbf6bd58863aac2b1a7a7826d88abd54b
Anass Ouazri <78270875+Aouazri@users.noreply.github.com>
2021-12-14 23:58:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ from replit import clear
 #I need a function that updates score when the user is right 
 # the loser between the two choices become option A 
 #while the user is right, we keep playing
-#I need a function to randomly chooses someone from the list
-def random_choice(data):
-  choice= random.choice(data)
-  return choice
+#I need to randomly chooses someone from the list
 # ******************************* #
 #function that compares who has more followers and updates the score accordingly:# 
 def compare(dict1,dict2,user_answer):
@@ -33,10 +30,10 @@ def answer(dict1,dict2,user_answer):
 # ******************************* ******************************* **********#
 
 print(logo)
-first_choice= random_choice(data)
-second_choice= random_choice(data)
+first_choice= random.choice(data)
+second_choice= random.choice(data)
 if first_choice == second_choice:
-  second_choice= random_choice(data)
+  second_choice= random.choice(data)
 score= 0
 should_continue=True
 #while will be here
@@ -56,7 +53,7 @@ while should_continue==True:
     
     #switchero magic
     first_choice= second_choice
-    second_choice= random_choice(data)
+    second_choice= random.choice(data)
     
   else:
     clear()
